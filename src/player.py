2e37dfc22abe8d776f29a497e2f5392f863304ef
csanahuja <csanahuja10@gmail.com>
2019-12-26 20:35:52 +0100

    Make work broadcast and more impl

---
+++ src/player.py
@@ -0,0 +1,22 @@
+class Player:
+
+    def __init__(self, index, name=None, colour=None, gender=None, credit=1000):
+        self.name = self.assign_name(name, index)
+        self.colour = self.assign_colour(colour)
+        self.gender = self.assign_gender(gender)
+        self.credit = credit
+
+    def assign_name(self, name, index):
+        if not name:
+            return f"Player - {index}"
+        return name
+
+    def assign_colour(self, colour):
+        if not colour:
+            return '#9cfff6'
+        return colour
+
+    def assign_gender(self, gender):
+        if not gender:
+            return 'Man'
+        return gender
--- src/server.py
+++ src/server.py
@@ -1,48 +1,87 @@
 from autobahn.asyncio.websocket import WebSocketServerFactory
 from autobahn.asyncio.websocket import WebSocketServerProtocol
 from monopoly import Monopoly
+from player import Player
 
 import asyncio
-import ssl
+import json
 
 
 class MonopolyServerProtocol(WebSocketServerProtocol):
 
     def onConnect(self, request):
         print('Client connecting: {0}'.format(request.peer))
-        print('Initializing monopoly')
-        self.monopoly = Monopoly()
 
     def onOpen(self):
         print("WebSocket connection open.")
+        self.factory.register_client(self)
 
     def onMessage(self, payload, isBinary):
-        payload = payload.decode('utf-8')
-        print('Message received: {0}'.format(payload))
-        if payload == 'UPDATE_CREDIT':
-            self.monopoly.credit += 100
-        if payload == 'CLEAR_CREDIT':
-            self.monopoly.credit = 0
-        print('Current credit: {0}'.format(self.monopoly.credit))
-        self.sendMessage(str(self.monopoly.credit).encode('utf-8'), isBinary)
+        payload = payload.decode('utf8')
+        payload = payload.split('|')
+        action = payload[0]
+
+        if action == 'ADD_PLAYER':
+            data = json.loads(payload[1])
+            self.factory.add_player(data, self)
+
+        # msg = "{} from {}".format(payload.decode('utf8'), self.peer)
+        # self.factory.broadcast(msg)
+
+        # payload = payload.decode('utf-8')
+        # print('Message received: {0}'.format(payload))
+        # if payload == 'UPDATE_CREDIT':
+        #     self.monopoly.credit += 100
+        # if payload == 'CLEAR_CREDIT':
+        #     self.monopoly.credit = 0
+        # self.sendMessage(str(self.monopoly.credit).encode('utf-8'), isBinary)
 
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {0}".format(reason))
+        self.factory.unregister_client(self)
+
+
+class MonopolyServerFactory(WebSocketServerFactory):
+
+    def __init__(self):
+        super().__init__()
+        self.clients = []
+        self.players = {}
+        self.num_players = 0
+        self.monopoly = Monopoly()
+
+    def register_client(self, client):
+        if client not in self.clients:
+            self.clients.append(client)
+
+    def add_player(self, player_info, client):
+        if client.peer not in self.players:
+            player = Player(index = self.num_players + 1, **player_info)
+            self.players[client.peer] = player
+            self.num_players += 1
+            self.broadcast('Player Added')
+
+    def unregister_client(self, client):
+        if client in self.clients:
+            print("unregistered client {}".format(client.peer))
+            self.clients.remove(client)
+            self.players.pop(client.peer, None)
+
+    def broadcast(self, msg):
+        print("broadcasting prepared message '{}' ..".format(msg))
+        for c in self.clients:
+            preparedMsg = self.prepareMessage(msg.encode('utf-8'))
+            c.sendPreparedMessage(preparedMsg)
+            print("prepared message sent to {}".format(c.peer))
 
 
 if __name__ == '__main__':
 
-    factory = WebSocketServerFactory()
+    factory = MonopolyServerFactory()
     factory.protocol = MonopolyServerProtocol
 
-    sslcontext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    sslcontext.load_cert_chain(
-        '/etc/httpd/certificates/niticras.crt',
-        '/etc/httpd/certificates/niticras.key',
-    )
-
     loop = asyncio.get_event_loop()
-    coro = loop.create_server(factory, '0.0.0.0', 9000, ssl=sslcontext)
+    coro = loop.create_server(factory, '0.0.0.0', 9000)
     server = loop.run_until_complete(coro)
 
     try:
