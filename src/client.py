342d8deeb6696b54303df0fea890d4c68b87e252
csanahuja <csanahuja10@gmail.com>
2019-12-26 03:05:05 +0100

    Initial server/client testing

--- src/client.py
+++ src/client.py
@@ -1,4 +1,7 @@
 from autobahn.asyncio.websocket import WebSocketClientProtocol
+from autobahn.asyncio.websocket import WebSocketClientFactory
+
+import asyncio
 
 
 class MyClientProtocol(WebSocketClientProtocol):
@@ -15,18 +18,11 @@ class MyClientProtocol(WebSocketClientProtocol):
 
 if __name__ == '__main__':
 
-   try:
-      import asyncio
-   except ImportError:
-      ## Trollius >= 0.3 was renamed
-      import trollius as asyncio
-
-   from autobahn.asyncio.websocket import WebSocketClientFactory
    factory = WebSocketClientFactory()
    factory.protocol = MyClientProtocol
 
    loop = asyncio.get_event_loop()
-   coro = loop.create_connection(factory, '127.0.0.1', 9000)
+   coro = loop.create_connection(factory, '0.0.0.0', 9000)
    loop.run_until_complete(coro)
    loop.run_forever()
    loop.close()
--- src/server.py
+++ src/server.py
@@ -1,42 +1,43 @@
+from autobahn.asyncio.websocket import WebSocketServerFactory
 from autobahn.asyncio.websocket import WebSocketServerProtocol
+
+import asyncio
 
 
 class MyServerProtocol(WebSocketServerProtocol):
 
-   def onMessage(self, payload, isBinary):
-      ## echo back message verbatim
-      self.sendMessage(payload, isBinary)
+    def onConnect(self, request):
+        print("Client connecting: {0}".format(request.peer))
 
-   def onConnect(self, request):
-      print("Client connecting: {}".format(request.peer))
+    def onOpen(self):
+        print("WebSocket connection open.")
 
-   def onOpen(self):
-      print("WebSocket connection open.")
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            print("Binary message received: {0} bytes".format(len(payload)))
+        else:
+            print("Text message received: {0}".format(payload.decode('utf8')))
 
-   def onClose(self, wasClean, code, reason):
-      print("WebSocket connection closed: {}".format(reason))
+        # echo back message verbatim
+        self.sendMessage(payload, isBinary)
+
+    def onClose(self, wasClean, code, reason):
+        print("WebSocket connection closed: {0}".format(reason))
 
 
 if __name__ == '__main__':
 
-   try:
-      import asyncio
-   except ImportError:
-      ## Trollius >= 0.3 was renamed
-      import trollius as asyncio
+    factory = WebSocketServerFactory()
+    factory.protocol = MyServerProtocol
 
-   from autobahn.asyncio.websocket import WebSocketServerFactory
-   factory = WebSocketServerFactory()
-   factory.protocol = MyServerProtocol
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(factory, '0.0.0.0', 9000)
+    server = loop.run_until_complete(coro)
 
-   loop = asyncio.get_event_loop()
-   coro = loop.create_server(factory, '127.0.0.1', 9000)
-   server = loop.run_until_complete(coro)
-
-   try:
-      loop.run_forever()
-   except KeyboardInterrupt:
-      pass
-   finally:
-      server.close()
-      loop.close()
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.close()
